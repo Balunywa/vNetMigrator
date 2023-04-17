26ccd49b19847e565fd1131da7cf594ef7402073
Balunywa <Lukman@taacs.cloud>
2023-04-17 16:26:33 -0400

    updates

--- app.py
+++ app.py
@@ -1,165 +1,95 @@
+from flask import Flask, render_template
+
 from flask import Flask, render_template, request, jsonify
+#from azure_sdk_migration import migrate_resources, rollback_resources
 from azure.mgmt.network import NetworkManagementClient
 from azure.identity import DefaultAzureCredential
+from azure.mgmt.subscription import SubscriptionClient
 from azure.mgmt.resource import ResourceManagementClient
-from flask_sqlalchemy import SQLAlchemy
-import datetime
-import config
 
 app = Flask(__name__)
 
+def migrate_vnet_to_vwan_hub(subscription_id, spkrg, spkvnetname, spkpeeringname, vwanrg, vhubname):
+    # Authenticate and create network management client
+    credential = DefaultAzureCredential()
+    network_client = NetworkManagementClient(credential, subscription_id)
 
-app.config['SECRET_KEY'] = 'mysecretkey'
-app.config['SQLALCHEMY_DATABASE_URI'] = conn_str
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = False
+    # Get Spoke VNET ResourceID
+    vnet = network_client.virtual_networks.get(spkrg, spkvnetname)
+    vnetid = vnet.id
 
-db = SQLAlchemy(app)
+    # Set UseRemoteGateways to False
+    network_client.virtual_network_peerings.begin_update(
+        spkrg,
+        spkvnetname,
+        spkpeeringname,
+        {
+            'use_remote_gateways': False
+        }
+    )
 
-class Migration(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    resource_group_name = db.Column(db.String(100), nullable=False)
-    vnet_name = db.Column(db.String(100), nullable=False)
-    status = db.Column(db.String(50), nullable=False)
-    migration_date = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
+    # Configure VWAN HUB Virtual Network Connection
+    network_client.hub_virtual_network_connections.begin_create_or_update(
+        vwanrg,
+        vhubname,
+        spkvnetname,
+        {
+            'remote_virtual_network': {
+                'id': vnetid
+            }
+        }
+    )
 
-db.create_all()
 
+def get_subscriptions():
+    credential = DefaultAzureCredential()
+    subscription_client = SubscriptionClient(credential)
+    subscriptions = subscription_client.subscriptions.list()
+    return [sub.as_dict() for sub in subscriptions]
+
+def get_vnets(subscription_id):
+    credential = DefaultAzureCredential()
+    resource_client = ResourceManagementClient(credential, subscription_id)
+    vnets = resource_client.resources.list_by_resource_group(filter="resourceType eq 'Microsoft.Network/virtualNetworks'")
+    return [vnet.as_dict() for vnet in vnets]
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/subscriptions', methods=['GET'])
-def get_subscriptions():
-    try:
-        subscriptions = list_subscriptions()
-        return jsonify({'success': True, 'subscriptions': subscriptions})
-    except Exception as e:
-        return jsonify({'success': False, 'message': f'Error: {str(e)}'})
+def subscriptions():
+    subs = get_subscriptions()
+    return jsonify(subs)
 
-@app.route('/vpn_vnets', methods=['GET'])
-def get_vpn_vnets():
-    subscription_id = request.args.get('subscription_id', '')
-    if not subscription_id:
-        return jsonify({'success': False, 'message': 'No subscription_id provided'})
-
-    try:
-        vpn_vnets = list_vpn_vnets(subscription_id)
-        return jsonify({'success': True, 'vpn_vnets': vpn_vnets})
-    except Exception as e:
-        return jsonify({'success': False, 'message': f'Error: {str(e)}'})
-
-# Add these new functions to perform the required tasks
-
-def list_subscriptions():
-    subscriptions = []
-    for subscription in resource_client.subscriptions.list():
-        subscriptions.append({
-            'subscription_id': subscription.subscription_id,
-            'display_name': subscription.display_name
-        })
-    return subscriptions
-
-def list_vpn_vnets(subscription_id):
-    # Set up the Azure clients with the specified subscription_id
-    network_client = NetworkManagementClient(credential, subscription_id)
-
-    vpn_vnets = []
-    for resource_group in resource_client.resource_groups.list():
-        for vnet in network_client.virtual_networks.list(resource_group.name):
-            if is_vnet_using_vpn_gateway(network_client, resource_group.name, vnet.name):
-                vpn_vnets.append({
-                    'resource_group': resource_group.name,
-                    'name': vnet.name
-                })
-    return vpn_vnets
+@app.route('/vnets', methods=['POST'])
+def vnets():
+    subscription_id = request.form['subscription_id']
+    vnets = get_vnets(subscription_id)
+    return jsonify(vnets)
+@app.route('/migrate', methods=['GET', 'POST'])
+def migrate():
+    if request.method == 'POST':
+        data = request.get_json()
+        migrate_vnet_to_vwan_hub(
+            data['subscription_id'],
+            data['spkrg'],
+            data['spkvnetname'],
+            data['spkpeeringname'],
+            data['vwanrg'],
+            data['vhubname']
+        )
+        return jsonify({'result': 'success'})
+    return render_template('migrate.html')
 
 
-
-def is_vnet_using_vpn_gateway(network_client, resource_group_name, vnet_name):
-    # Fetch all virtual network gateways in the resource group
-    gateways = network_client.virtual_network_gateways.list(resource_group_name)
-
-    for gateway in gateways:
-        if gateway.virtual_network.name == vnet_name and gateway.gateway_type == 'Vpn':
-            return True
-    return False
-
-@app.route('/migration_data', methods=['GET'])
-def get_migration_data():
-    migrations = Migration.query.all()
-    migration_list = []
-    for migration in migrations:
-        migration_list.append({
-            'resource_group_name': migration.resource_group_name,
-            'vnet_name': migration.vnet_name,
-            'status': migration.status,
-            'migration_date': migration.migration_date.strftime('%Y-%m-%d %H:%M:%S')
-        })
-    return jsonify({'success': True, 'migrations': migration_list})
-
-@app.route('/migrate', methods=['POST'])
-def migrate_vnet():
-    subscription_id = request.form.get('subscription_id', '')
-    resource_group_name = request.form.get('resource_group_name', '')
-    vnet_name = request.form.get('vnet_name', '')
-
-    if not (subscription_id and resource_group_name and vnet_name):
-        return jsonify({'success': False, 'message': 'Missing required information'})
-
-    # Perform the migration
-    try:
-        migrate_vnet_to_express_route(subscription_id, resource_group_name, vnet_name)
-        migration = Migration(resource_group_name=resource_group_name, vnet_name=vnet_name, status='Migrated')
-        db.session.add(migration)
-        db.session.commit()
-        return jsonify({'success': True, 'message': 'VNet migration successful'})
-    except Exception as e:
-        return jsonify({'success': False, 'message': f'Error: {str(e)}'})
-
-def migrate_vnet_to_express_route(subscription_id, resource_group_name, vnet_name):
-    # Set up the Azure clients with the specified subscription_id
-    network_client = NetworkManagementClient(credential, subscription_id)
-    
-    # Fetch the virtual network
-    vnet = network_client.virtual_networks.get(resource_group_name, vnet_name)
-
-    # Fetch the VPN Gateway associated with the virtual network
-    vpn_gateway = None
-    gateways = network_client.virtual_network_gateways.list(resource_group_name)
-    for gateway in gateways:
-        if gateway.virtual_network.name == vnet_name and gateway.gateway_type == 'Vpn':
-            vpn_gateway = gateway
-            break
-
-    if not vpn_gateway:
-        raise Exception(f"No VPN Gateway found for VNet '{vnet_name}'")
-
-    # Create an ExpressRoute Gateway with the same settings as the VPN Gateway
-    express_route_gateway_params = {
-        'location': vpn_gateway.location,
-        'sku': {
-            'name': 'Standard', # Choose the appropriate SKU based on the requirements
-            'tier': 'Standard'
-        },
-        'gateway_type': 'ExpressRoute',
-        'vpn_type': 'RouteBased',
-        'enable_bgp': vpn_gateway.enable_bgp,
-        'active_active': vpn_gateway.active_active,
-        'virtual_network': {
-            'id': vnet.id
-        }
-    }
-    express_route_gateway_creation = network_client.virtual_network_gateways.create_or_update(resource_group_name, f"{vnet_name}-ER-GW", express_route_gateway_params)
-    express_route_gateway_creation.wait()
-
-    # Delete the existing VPN Gateway
-    network_client.virtual_network_gateways.delete(resource_group_name, vpn_gateway.name).wait()
-
-    # Update the vNet connections to use the newly created ExpressRoute Gateway
-    connections = network_client.connections.list(resource_group_name)
-    for connection in connections:
-        if connection.virtual_network_gateway1.id == vpn_gateway.id:
-            connection.virtual_network_gateway1 = {
-                'id': express_route_gateway_creation.result().id
-            }
-            network_client.connections.create_or_update(resource_group_name, connection.name, connection).wait()
+@app.route('/rollback', methods=['GET', 'POST'])
+def rollback():
+    if request.method == 'POST':
+        data = request.get_json()
+        rollback_resources(data)
+        return jsonify({'result': 'success'})
+    return render_template('rollback.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
